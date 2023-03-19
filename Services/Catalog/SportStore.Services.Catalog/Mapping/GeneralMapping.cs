7594486dac1c628789fb6fa36fd02648523b4c7b
Shakir <shakirfarajullayev@gmail.com>
2023-03-19 23:29:25 +0400

    added generalmapping

---
+++ Services/Catalog/SportStore.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SportStore.Services.Catalog.Dtos;
+using SportStore.Services.Catalog.Models;
+
+namespace SportStore.Services.Catalog.Mapping;
+
+public class GeneralMapping : Profile
+{
+    public GeneralMapping()
+    {
+        CreateMap<Product, ProductDto>().ReverseMap();
+        CreateMap<Category, CategoryDto>().ReverseMap();
+        CreateMap<Feature, FeatureDto>().ReverseMap();
+
+        CreateMap<Product, ProductCreateDto>().ReverseMap();
+        CreateMap<Product, ProductUpdateDto>().ReverseMap();
+    }
+}
+
