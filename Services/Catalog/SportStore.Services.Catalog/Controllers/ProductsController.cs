8a805a8eada7d3521fac886a13662003c45122fe
Shakir <shakirfarajullayev@gmail.com>
2023-03-19 23:25:54 +0400

    controller added

---
+++ Services/Catalog/SportStore.Services.Catalog/Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Services.Catalog.Dtos;
+using SportStore.Services.Catalog.Services;
+using SportStore.Shared.ControllerBases;
+
+namespace SportStore.Services.Catalog.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : CustomBaseController
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var response = await _productService.GetAllAsync();
+        return CreateActionResultInstance(response);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(string id)
+    {
+        var response = await _productService.GetByIdAsync(id);
+        return CreateActionResultInstance(response);
+    }
+
+    [HttpGet]
+    [Route("/api/[controller]/GetAllByUserId/{userId}")]
+    public async Task<IActionResult> GetAllByUserId(string userId)
+    {
+        var response = await _productService.GetAllByUserIdAsync(userId);
+        return CreateActionResultInstance(response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(ProductCreateDto productCreateDto)
+    {
+        var response = await _productService.CreateAsync(productCreateDto);
+        return CreateActionResultInstance(response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update(ProductUpdateDto productUpdateDto)
+    {
+        var response = await _productService.UpdateAsync(productUpdateDto);
+        return CreateActionResultInstance(response);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var response = await _productService.DeleteAsync(id);
+        return CreateActionResultInstance(response);
+    }
+}
+
