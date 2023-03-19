048e0963d97d28f5e01119928423c9de7f679740
Shakir <shakirfarajullayev@gmail.com>
2023-03-19 23:26:26 +0400

    added services

---
+++ Services/Catalog/SportStore.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using SportStore.Services.Catalog.Dtos;
+using SportStore.Shared.Dtos;
+
+namespace SportStore.Services.Catalog.Services;
+
+public interface ICategoryService
+{
+    Task<Response<List<CategoryDto>>> GetAllAsync();
+    Task<Response<CategoryDto>> CreateAsync(CategoryDto categoryDto);
+    Task<Response<CategoryDto>> GetByIdAsync(string id);
+}
+
---
+++ Services/Catalog/SportStore.Services.Catalog/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using SportStore.Services.Catalog.Dtos;
+using SportStore.Shared.Dtos;
+
+namespace SportStore.Services.Catalog.Services;
+
+public interface IProductService
+{
+    Task<Response<List<ProductDto>>> GetAllAsync();
+    Task<Response<ProductDto>> GetByIdAsync(string id);
+    Task<Response<List<ProductDto>>> GetAllByUserIdAsync(string userId);
+    Task<Response<ProductDto>> CreateAsync(ProductCreateDto productCreateDto);
+    Task<Response<NoContent>> UpdateAsync(ProductUpdateDto productUpdateDto);
+    Task<Response<NoContent>> DeleteAsync(string id);
+
+}
+
