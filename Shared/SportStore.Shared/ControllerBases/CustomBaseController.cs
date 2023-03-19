134e959e2fcd84ad617e25f5729c8745a4ba99bb
Shakir <shakirfarajullayev@gmail.com>
2023-03-19 23:31:24 +0400

    added CustomBaseController

---
+++ Shared/SportStore.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Shared.Dtos;
+
+namespace SportStore.Shared.ControllerBases;
+
+public class CustomBaseController : ControllerBase
+{
+    public IActionResult CreateActionResultInstance<T>(Response<T> response)
+    {
+        return new ObjectResult(response)
+        {
+            StatusCode = response.StatusCode
+        };
+    }
+}
+
