14ec74e166b2c5a1a8d8494143bb8b2d31b153f7
Shakir <shakirfarajullayev@gmail.com>
2023-03-19 23:28:23 +0400

    added settings

---
+++ Services/Catalog/SportStore.Services.Catalog/Settings/DatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace SportStore.Services.Catalog.Settings;
+
+public class DatabaseSettings : IDatabaseSettings
+{
+    public string? ProductColletionName { get; set; }
+    public string? CategoryCollectionName { get; set; }
+    public string? ConnectionString { get; set; }
+    public string? DatabaseName { get; set; }
+}
+
---
+++ Services/Catalog/SportStore.Services.Catalog/Settings/IDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace SportStore.Services.Catalog.Settings;
+
+public interface IDatabaseSettings
+{
+    public string? ProductColletionName { get; set; }
+    public string? CategoryCollectionName { get; set; }
+    public string? ConnectionString { get; set; }
+    public string? DatabaseName { get; set; }
+}
+
