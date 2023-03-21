6e2c48691fa9304e38fb38be8868bf654673ede6
Shakir <shakirfarajullayev@gmail.com>
2023-03-21 16:37:43 +0400

    Add Required

--- IdentityServer/SportStore.IdentityServer/Dtos/SignUpDto.cs
+++ IdentityServer/SportStore.IdentityServer/Dtos/SignUpDto.cs
@@ -1,10 +1,16 @@
-﻿namespace SportStore.IdentityServer.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportStore.IdentityServer.Dtos
 {
     public class SignUpDto
     {
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string City { get; set; }
     }
 }
