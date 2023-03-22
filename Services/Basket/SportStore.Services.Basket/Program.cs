2444901a58b180d78f5afff8c72f9faf76be0dd4
Shakir <shakirfarajullayev@gmail.com>
2023-03-23 00:28:31 +0400

    add services

---
+++ Services/Basket/SportStore.Services.Basket/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.Extensions.Options;
+using SportStore.Services.Basket.Services;
+using SportStore.Services.Basket.Settings;
+using SportStore.Shared.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+builder.Services.AddScoped<IBasketService, BasketService>();
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
+
+builder.Services.AddSingleton<RedisService>(sp =>
+{
+    var redisSetttings = sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+
+    var redis = new RedisService(redisSetttings.Host, redisSetttings.Port);
+
+    redis.Connect();
+    return redis;
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
