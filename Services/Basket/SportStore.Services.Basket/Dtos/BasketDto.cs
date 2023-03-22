5af0c9534066b23c4d70713f804055401f606fe8
Shakir <shakirfarajullayev@gmail.com>
2023-03-23 00:26:03 +0400

    BasketDto

---
+++ Services/Basket/SportStore.Services.Basket/Dtos/BasketDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SportStore.Services.Basket.Dtos;
+
+public class BasketDto
+{
+    public string UserId { get; set; } = null!;
+    public string? DiscountCode { get; set; }
+    public List<BasketItemDto> BasketItems { get; set; } = null!;
+
+    public decimal TotalPrice
+    {
+        get => BasketItems.Sum(b => b.Price * b.Quantity);
+    }
+}
+
