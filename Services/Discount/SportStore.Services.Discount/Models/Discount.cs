a74324eec2e32e337f43230dcc9ec5317057065f
Shakir <shakirfarajullayev@gmail.com>
2023-03-23 18:53:01 +0400

    add model

---
+++ Services/Discount/SportStore.Services.Discount/Models/Discount.cs
@@ -0,0 +1,13 @@
+﻿namespace SportStore.Services.Discount.Models;
+
+
+[Dapper.Contrib.Extensions.Table("discount")]
+public class Discount
+{
+    public int Id { get; set; }
+    public string UserId { get; set; } = null!;
+    public int Rate { get; set; }
+    public string? Code { get; set; }
+    public DateTime CreatedTime { get; set; }
+}
+
