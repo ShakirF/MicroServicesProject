8dbd0d8d11a696704804e9d09a717971235b9f87
Shakir <shakirfarajullayev@gmail.com>
2023-03-25 00:56:55 +0400

    add mapping

---
+++ Services/Order/SportStore.Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SportStore.Services.Order.Application.Dtos;
+using SportStore.Services.Order.Domain.OrderAggregate;
+
+namespace SportStore.Services.Order.Application.Mapping;
+
+public class CustomMapping : Profile
+{
+    public CustomMapping()
+    {
+        CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+        CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+        CreateMap<Address, AddressDto>().ReverseMap();
+
+    }
+}
+
