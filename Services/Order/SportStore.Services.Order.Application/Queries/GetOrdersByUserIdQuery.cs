8727cf708fa268d10fc795181191a868ae7b45da
Shakir <shakirfarajullayev@gmail.com>
2023-03-25 00:56:45 +0400

    add queries

---
+++ Services/Order/SportStore.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using SportStore.Services.Order.Application.Dtos;
+using SportStore.Shared.Dtos;
+
+namespace SportStore.Services.Order.Application.Queries;
+
+public class GetOrdersByUserIdQuery : IRequest<Response<List<OrderDto>>>
+{
+    public string UserId { get; set; }
+}
+
