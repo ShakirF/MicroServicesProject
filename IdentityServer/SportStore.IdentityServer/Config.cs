6b3111328b26cc24b6c6a8ca4b861067382bbed1
Shakir <shakirfarajullayev@gmail.com>
2023-03-25 16:34:33 +0400

    add resource_gateway

--- IdentityServer/SportStore.IdentityServer/Config.cs
+++ IdentityServer/SportStore.IdentityServer/Config.cs
@@ -18,6 +18,8 @@ namespace SportStore.IdentityServer
             new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
             new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
             new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
+            new ApiResource("resource_payment"){Scopes={"payment_fullpermission"}},
+            new ApiResource("resource_gateway"){Scopes={"gateway_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -39,7 +41,9 @@ namespace SportStore.IdentityServer
                 new ApiScope("photo_stock_fullpermission","Photo Stock API ucun full access"),
                 new ApiScope("basket_fullpermission","Basket API ucun full access"),
                 new ApiScope("discount_fullpermission","Discount API ucun full access"),
-                 new ApiScope("order_fullpermission","Order API ucun full access"),
+                new ApiScope("order_fullpermission","Order API ucun full access"),
+                new ApiScope("payment_fullpermission","Payment API ucun full access"),
+                new ApiScope("gateway_fullpermission","Gateway API ucun full access"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -52,7 +56,7 @@ namespace SportStore.IdentityServer
                     ClientId = "WebMvcClient",
                     ClientSecrets = {new Secret("secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    AllowedScopes = { "catalog_fullpermission", "photo_stock_fullpermission",
+                    AllowedScopes = { "catalog_fullpermission", "photo_stock_fullpermission","gateway_fullpermission",
                         IdentityServerConstants.LocalApi.ScopeName }
                 },
                 new Client
@@ -62,7 +66,8 @@ namespace SportStore.IdentityServer
                     AllowOfflineAccess = true,
                     ClientSecrets = {new Secret("secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes = {"basket_fullpermission","discount_fullpermission","order_fullpermission",
+                    AllowedScopes = {"basket_fullpermission","discount_fullpermission",
+                        "order_fullpermission","payment_fullpermission","gateway_fullpermission",
                         IdentityServerConstants.StandardScopes.Email,
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
