0e2c1243fd28cda12aeaf90669729d7e2573b86c
Shakir <shakirfarajullayev@gmail.com>
2023-03-25 16:35:46 +0400

    add payment service

---
+++ Services/FakePayment/SportStore.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Shared.ControllerBases;
+using SportStore.Shared.Dtos;
+
+namespace SportStore.Services.FakePayment.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FakePaymentsController : CustomBaseController
+{
+    [HttpPost]
+    public IActionResult ReceivePaymnet()
+    {
+        return CreateActionResultInstance(Response<NoContent>.Success(200));
+    }
+}
+
