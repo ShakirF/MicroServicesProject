93df0c21f9365b95469f3343ce07851f82a5530d
Shakir <shakirfarajullayev@gmail.com>
2023-03-26 22:06:47 +0400

    add services

---
+++ Frontends/SportStore.Web/Services/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using SportStore.Web.Models;
+
+namespace SportStore.Web.Services.Interfaces;
+
+public interface IUserService
+{
+    Task<UserViewModel> GetUser();
+
+}
+
---
+++ Frontends/SportStore.Web/Services/UserService.cs
@@ -0,0 +1,20 @@
+﻿using SportStore.Web.Models;
+using SportStore.Web.Services.Interfaces;
+
+namespace SportStore.Web.Services;
+
+public class UserService : IUserService
+{
+    private readonly HttpClient _httpClient;
+
+    public UserService(HttpClient httpClient)
+    {
+        this._httpClient = httpClient;
+    }
+
+    public async Task<UserViewModel> GetUser()
+    {
+        return await _httpClient.GetFromJsonAsync<UserViewModel>("/api/user/getuser");
+    }
+}
+
