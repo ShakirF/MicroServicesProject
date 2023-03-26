bb0a4ee327f26670499a4a67d9040878482a2030
Shakir <shakirfarajullayev@gmail.com>
2023-03-26 16:19:06 +0400

    Add Mvc project

---
+++ Frontends/SportStore.Web/Models/SigninInput.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportStore.Web.Models;
+
+public class SigninInput
+{
+    [Display(Name = "Email adresiniz")]
+    public string Email { get; set; } = null!;
+
+    [Display(Name = "Shifreniz")]
+    public string Password { get; set; } = null!;
+
+    [Display(Name = "Meni xatirla")]
+    public bool IsRemember { get; set; }
+}
+
---
+++ Frontends/SportStore.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,15 @@
+﻿using IdentityModel.Client;
+using SportStore.Shared.Dtos;
+using SportStore.Web.Models;
+
+namespace SportStore.Web.Services.Interfaces;
+
+public interface IIdentityService
+{
+    Task<Response<bool>> SignIn(SigninInput signinInput);
+
+    Task<TokenResponse> GetAccesTokenByRefreshToken();
+
+    Task RevokeRefreshToken();
+}
+
