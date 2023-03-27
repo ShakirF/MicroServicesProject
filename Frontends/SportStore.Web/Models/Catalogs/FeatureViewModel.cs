8d6da74ec389a2a6d53c3c51bc4913cba16c2533
Shakir <shakirfarajullayev@gmail.com>
2023-03-28 01:39:04 +0400

    add atribute

--- Frontends/SportStore.Web/Models/Catalogs/FeatureViewModel.cs
+++ Frontends/SportStore.Web/Models/Catalogs/FeatureViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace SportStore.Web.Models.Catalogs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportStore.Web.Models.Catalogs;
 
 public class FeatureViewModel
 {
+
+    [Display(Name = "Məhsulun ölçüsü")]
+    [Required]
     public int Size { get; set; }
 }
 
--- Frontends/SportStore.Web/Models/Catalogs/ProductCreateInput.cs
+++ Frontends/SportStore.Web/Models/Catalogs/ProductCreateInput.cs
@@ -1,14 +1,27 @@
-﻿namespace SportStore.Web.Models.Catalogs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportStore.Web.Models.Catalogs;
 
 public class ProductCreateInput
 {
+    [Display(Name = "Məhsulun adı")]
+    [Required]
     public string Name { get; set; } = null!;
+
+    [Display(Name = "Məhsulun açıqlaması")]
+    [Required]
     public string? Description { get; set; }
+
+    [Display(Name = "Məhsulun qiyməti")]
+    [Required]
     public decimal Price { get; set; }
     public string? Picture { get; set; }
     public string? UserId { get; set; }
     public FeatureViewModel? Feature { get; set; }
 
+
+    [Display(Name = "Məhsulun kateqoriyası")]
+    [Required]
     public string CategoryId { get; set; } = null!;
 }
 
