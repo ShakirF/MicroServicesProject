664a6b58dd42d707cb760f832ad735908aadc5cd
Shakir <shakirfarajullayev@gmail.com>
2023-03-27 19:01:02 +0400

    add handler

---
+++ Frontends/SportStore.Web/Handler/ResourceOwnerPasswordTokenHandler.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using SportStore.Web.Exceptions;
+using SportStore.Web.Services.Interfaces;
+
+namespace SportStore.Web.Handler;
+
+public class ResourceOwnerPasswordTokenHandler : DelegatingHandler
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IIdentityService _identityService;
+    private readonly ILogger<ResourceOwnerPasswordTokenHandler> _logger;
+
+    public ResourceOwnerPasswordTokenHandler(
+        IHttpContextAccessor httpContextAccessor,
+        IIdentityService identityService,
+        ILogger<ResourceOwnerPasswordTokenHandler> logger)
+    {
+        this._httpContextAccessor = httpContextAccessor;
+        this._identityService = identityService;
+        this._logger = logger;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var accessToken = await _httpContextAccessor.HttpContext.GetTokenAsync
+            (OpenIdConnectParameterNames.AccessToken);
+
+        request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+
+        var response = await base.SendAsync(request, cancellationToken);
+        if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+        {
+            var tokenResponse = await _identityService.GetAccesTokenByRefreshToken();
+
+            if (tokenResponse == null)
+            {
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+
+                response = await base.SendAsync(request, cancellationToken);
+            }
+        }
+        if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+        {
+            throw new UnAuthorizeException();
+        }
+
+        return response;
+    }
+
+}
+
