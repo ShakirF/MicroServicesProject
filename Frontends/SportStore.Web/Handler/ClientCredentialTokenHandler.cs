a55bda770c44f706fb9d8163c6195db1128905b4
Shakir <shakirfarajullayev@gmail.com>
2023-03-28 01:38:35 +0400

    add ClientCredentialTokenHandler

---
+++ Frontends/SportStore.Web/Handler/ClientCredentialTokenHandler.cs
@@ -0,0 +1,31 @@
+﻿using SportStore.Web.Exceptions;
+using SportStore.Web.Services.Interfaces;
+using System.Net.Http.Headers;
+
+namespace SportStore.Web.Handler;
+
+public class ClientCredentialTokenHandler : DelegatingHandler
+{
+    private readonly IClientCredentialTokenService _clientCredentialTokenService;
+
+    public ClientCredentialTokenHandler(IClientCredentialTokenService clientCredentialTokenService)
+    {
+        this._clientCredentialTokenService = clientCredentialTokenService;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        request.Headers.Authorization = new AuthenticationHeaderValue
+            ("Bearer", await _clientCredentialTokenService.GetToken());
+
+        var response = await base.SendAsync(request, cancellationToken);
+
+        if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+        {
+            throw new UnAuthorizeException();
+        }
+
+        return response;
+    }
+}
+
