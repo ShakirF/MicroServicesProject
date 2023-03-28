c808c709880fe38d144926c05bd2e894e0679acf
Shakir <shakirfarajullayev@gmail.com>
2023-03-28 17:16:23 +0400

    ShortDescription

--- Frontends/SportStore.Web/Models/Catalogs/ProductViewModel.cs
+++ Frontends/SportStore.Web/Models/Catalogs/ProductViewModel.cs
@@ -5,8 +5,7 @@ public class ProductViewModel
     public string Id { get; set; } = null!;
     public string Name { get; set; } = null!;
     public string? Description { get; set; }
-
-
+    public string ShortDescription { get => Description.Length > 100 ? Description.Substring(0, 100) + "..." : Description; }
     public decimal Price { get; set; }
 
     public string? UserId { get; set; }
