14a12472445464b614a9bfcb2cca657b9c3ab9b8
Shakir <shakirfarajullayev@gmail.com>
2023-03-28 22:49:26 +0400

    IBasketService

---
+++ Frontends/SportStore.Web/Services/Interfaces/IBasketService.cs
@@ -0,0 +1,21 @@
+﻿using SportStore.Web.Models.Baskets;
+
+namespace SportStore.Web.Services.Interfaces;
+
+public interface IBasketService
+{
+    Task<bool> SaveOrUpdate(BasketViewModel basketViewModel);
+
+    Task<BasketViewModel> Get();
+
+    Task<bool> Delete();
+
+    Task AddBasketItem(BasketItemViewModel basketItemViewModel);
+
+    Task<bool> RemoveBasketItem(string productId);
+
+    Task<bool> ApplyDiscount(string discountCode);
+
+    Task<bool> CancelApplyDiscount();
+}
+
