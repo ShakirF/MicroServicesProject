4cb2b8b463612e465c298f04e15a9649bea241da
Shakir <shakirfarajullayev@gmail.com>
2023-03-28 17:15:18 +0400

    GetAllProductAsync

--- Frontends/SportStore.Web/Controllers/HomeController.cs
+++ Frontends/SportStore.Web/Controllers/HomeController.cs
@@ -1,27 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportStore.Web.Models;
+using SportStore.Web.Services.Interfaces;
 using System.Diagnostics;
 
 namespace SportStore.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICatalogService _catalogService;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICatalogService catalogService)
         {
             _logger = logger;
+            _catalogService = catalogService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _catalogService.GetAllProductAsync());
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
