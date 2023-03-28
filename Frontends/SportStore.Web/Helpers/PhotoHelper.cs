2290d5faa9172048b48af4c986fcabf83e46c5e5
Shakir <shakirfarajullayev@gmail.com>
2023-03-28 17:15:33 +0400

    PhotoHelper

---
+++ Frontends/SportStore.Web/Helpers/PhotoHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+using SportStore.Web.Models;
+
+namespace SportStore.Web.Helpers;
+
+public class PhotoHelper
+{
+    private readonly ServiceApiSettings _serviceApiSettings;
+
+    public PhotoHelper(IOptions<ServiceApiSettings> serviceApiSettings)
+    {
+        this._serviceApiSettings = serviceApiSettings.Value;
+    }
+
+    public string GetPhotoStockUrl(string photoUrl)
+    {
+        return $"{_serviceApiSettings.PhotoStockUri}/photos/{photoUrl}";
+
+
+    }
+}
