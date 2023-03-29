d157eb05d1627c8dc40c5d25b63d1a89b5d07b35
Shakir <shakirfarajullayev@gmail.com>
2023-03-29 17:51:53 +0400

    DiscountViewModel

---
+++ Frontends/SportStore.Web/Models/Discounts/DiscountViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SportStore.Web.Models.Discounts;
+
+public class DiscountViewModel
+{
+    public string UserId { get; set; } = null!;
+    public int Rate { get; set; }
+    public string? Code { get; set; }
+
+}
+
