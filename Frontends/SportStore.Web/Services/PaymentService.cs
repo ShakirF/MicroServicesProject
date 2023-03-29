47df56ba078609a5a0c55bad75789da22b3ada0c
Shakir <shakirfarajullayev@gmail.com>
2023-03-29 17:53:28 +0400

    PaymentService

---
+++ Frontends/SportStore.Web/Services/PaymentService.cs
@@ -0,0 +1,22 @@
+﻿using SportStore.Web.Models.FakePayments;
+using SportStore.Web.Services.Interfaces;
+
+namespace SportStore.Web.Services;
+
+public class PaymentService : IPaymentService
+{
+    private readonly HttpClient _httpClient;
+
+    public PaymentService(HttpClient httpClient)
+    {
+        this._httpClient = httpClient;
+    }
+
+    public async Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput)
+    {
+        var response = await _httpClient.PostAsJsonAsync<PaymentInfoInput>("fakepayments", paymentInfoInput);
+
+        return response.IsSuccessStatusCode;
+    }
+}
+
