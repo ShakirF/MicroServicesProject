7a44f8d29c8d78425d6b01b7793baa575aaab75e
Shakir <shakirfarajullayev@gmail.com>
2023-03-29 17:56:01 +0400

    DiscountRate

--- Services/Basket/SportStore.Services.Basket/Dtos/BasketDto.cs
+++ Services/Basket/SportStore.Services.Basket/Dtos/BasketDto.cs
@@ -4,6 +4,7 @@ public class BasketDto
 {
     public string UserId { get; set; } = null!;
     public string? DiscountCode { get; set; }
+    public int? DiscountRate { get; set; }
     public List<BasketItemDto> BasketItems { get; set; } = null!;
 
     public decimal TotalPrice
