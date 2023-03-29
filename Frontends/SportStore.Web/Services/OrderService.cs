f9ff8bd39ae3318354f24677ffc654b4531c3d8e
Shakir <shakirfarajullayev@gmail.com>
2023-03-29 22:53:20 +0400

    OrderService

--- Frontends/SportStore.Web/Services/OrderService.cs
+++ Frontends/SportStore.Web/Services/OrderService.cs
@@ -1,18 +1,87 @@
-﻿using SportStore.Web.Models.Orders;
+﻿using SportStore.Shared.Dtos;
+using SportStore.Shared.Services;
+using SportStore.Web.Models.FakePayments;
+using SportStore.Web.Models.Orders;
 using SportStore.Web.Services.Interfaces;
 
 namespace SportStore.Web.Services;
 
 public class OrderService : IOrderService
 {
-    public Task<OrderCreatedViewModel> CreateOrder(CheckoutInfoInput checkoutInfoInput)
+    private readonly IPaymentService _paymentService;
+    private readonly HttpClient _httpClient;
+    private readonly IBasketService _basketService;
+    private readonly ISharedIdentityService _sharedIdentityService;
+
+    public OrderService(IPaymentService paymentService, HttpClient httpClient, IBasketService basketService, ISharedIdentityService sharedIdentityService)
     {
-        throw new NotImplementedException();
+        this._paymentService = paymentService;
+        this._httpClient = httpClient;
+        this._basketService = basketService;
+        this._sharedIdentityService = sharedIdentityService;
+
     }
 
-    public Task<List<OrderViewModel>> GetOrder()
+    public async Task<OrderCreatedViewModel> CreateOrder(CheckoutInfoInput checkoutInfoInput)
     {
-        throw new NotImplementedException();
+        var basket = await _basketService.Get();
+
+        var paymentInfoInput = new PaymentInfoInput()
+        {
+            CardName = checkoutInfoInput.CardName,
+            CardNumber = checkoutInfoInput.CardNumber,
+            Expiration = checkoutInfoInput.Expiration,
+            CVV = checkoutInfoInput.CVV,
+            TotalPrice = basket.TotalPrice
+        };
+
+        var responsePayment = _paymentService.ReceivePayment(paymentInfoInput);
+
+        if (!responsePayment.IsCompletedSuccessfully)
+        {
+            return new OrderCreatedViewModel() { Error = "Ödəmə alınmadı", IsSuccessful = false };
+        }
+
+        var orderCreateInput = new OrderCreateInput()
+        {
+            BuyerId = _sharedIdentityService.GetUserId,
+            Address = new AddressCreateInput
+            {
+                Province = checkoutInfoInput.Province,
+                District = checkoutInfoInput.District,
+                Street = checkoutInfoInput.Street,
+                ZipCode = checkoutInfoInput.ZipCode,
+                Line = checkoutInfoInput.Line
+            }
+        };
+        basket.BasketItems.ForEach(x =>
+        {
+            var orderItem = new OrderItemCreateInput
+            {
+                ProductId = x.ProductId,
+                Price = x.Price,
+                PictureUrl = "",
+                ProductName = x.ProductName
+            };
+            orderCreateInput.OrderItems.Add(orderItem);
+        });
+
+        var response = await _httpClient.PostAsJsonAsync<OrderCreateInput>("orders", orderCreateInput);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return new OrderCreatedViewModel() { Error = "Sifariş alınmadı", IsSuccessful = false };
+        }
+
+        return await response.Content.ReadFromJsonAsync<OrderCreatedViewModel>();
+
+
+    }
+
+    public async Task<List<OrderViewModel>> GetOrder()
+    {
+        var response = await _httpClient.GetFromJsonAsync<Response<List<OrderViewModel>>>("orders");
+        return response.Data;
     }
 
     public Task SuspendOrder(CheckoutInfoInput checkoutInfoInput)
