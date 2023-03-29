9f76fa67c9184299ea4c1e36e7f43efabe38eb06
Shakir <shakirfarajullayev@gmail.com>
2023-03-29 17:52:49 +0400

    IPaymentService

---
+++ Frontends/SportStore.Web/Services/Interfaces/IPaymentService.cs
@@ -0,0 +1,9 @@
+﻿using SportStore.Web.Models.FakePayments;
+
+namespace SportStore.Web.Services.Interfaces;
+
+public interface IPaymentService
+{
+    Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput);
+}
+
