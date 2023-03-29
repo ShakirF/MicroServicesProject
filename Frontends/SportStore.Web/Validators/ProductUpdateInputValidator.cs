882196ee1a3760a99677e1e254a0ff1f894fd406
Shakir <shakirfarajullayev@gmail.com>
2023-03-29 17:54:40 +0400

    ProductUpdateInputValidator

---
+++ Frontends/SportStore.Web/Validators/ProductUpdateInputValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using SportStore.Web.Models.Catalogs;
+
+namespace SportStore.Web.Validators;
+
+public class ProductUpdateInputValidator : AbstractValidator<ProductUpdateInput>
+{
+    public ProductUpdateInputValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("ad bos olmamalidir");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("aciqlama hissesi bos olmamalidir");
+        RuleFor(x => x.Feature.Size).InclusiveBetween(1, 100).WithMessage("olcu hissesi bos olmamalidir");
+        RuleFor(x => x.Price).NotEmpty().WithMessage("qiymet hissesi bos olmamalidir").ScalePrecision(2, 6).WithMessage("xetali pul formati");
+
+
+    }
+}
+
