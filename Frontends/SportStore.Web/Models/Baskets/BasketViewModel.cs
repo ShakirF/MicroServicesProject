54f5fdb9c4f36e2448b26c23546c5c4b3a55cdba
Shakir <shakirfarajullayev@gmail.com>
2023-03-29 17:50:35 +0400

    constructor

--- Frontends/SportStore.Web/Models/Baskets/BasketViewModel.cs
+++ Frontends/SportStore.Web/Models/Baskets/BasketViewModel.cs
@@ -2,10 +2,14 @@
 
 public class BasketViewModel
 {
+    public BasketViewModel()
+    {
+        _basketItems = new List<BasketItemViewModel>();
+    }
     public string UserId { get; set; } = null!;
     public string? DiscountCode { get; set; }
     public int? DiscountRate { get; set; }
-    private List<BasketItemViewModel> _basketItems { get; set; } = null!;
+    private List<BasketItemViewModel> _basketItems;
 
     public List<BasketItemViewModel> BasketItems
     {
@@ -34,7 +38,19 @@ public class BasketViewModel
 
     public bool HasDiscount
     {
-        get => !string.IsNullOrEmpty(DiscountCode);
+        get => !string.IsNullOrEmpty(DiscountCode) && DiscountRate.HasValue;
+    }
+
+    public void CancelDiscount()
+    {
+        DiscountCode = null;
+        DiscountRate = null;
+    }
+
+    public void ApplyDiscount(string code, int rate)
+    {
+        DiscountCode = code;
+        DiscountRate = rate;
     }
 }
 
