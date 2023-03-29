3a9075320f09fff956f2052cf47863b0dd09d568
Shakir <shakirfarajullayev@gmail.com>
2023-03-29 18:51:27 +0400

    OrderService

---
+++ Frontends/SportStore.Web/Services/OrderService.cs
@@ -0,0 +1,23 @@
+﻿using SportStore.Web.Models.Orders;
+using SportStore.Web.Services.Interfaces;
+
+namespace SportStore.Web.Services;
+
+public class OrderService : IOrderService
+{
+    public Task<OrderCreatedViewModel> CreateOrder(CheckoutInfoInput checkoutInfoInput)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<OrderViewModel>> GetOrder()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SuspendOrder(CheckoutInfoInput checkoutInfoInput)
+    {
+        throw new NotImplementedException();
+    }
+}
+
