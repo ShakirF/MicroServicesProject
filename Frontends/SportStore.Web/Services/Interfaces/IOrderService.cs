548c73d215eb100721fea27b10ee76235da40462
Shakir <shakirfarajullayev@gmail.com>
2023-03-29 18:51:17 +0400

    IOrderService

---
+++ Frontends/SportStore.Web/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,23 @@
+﻿using SportStore.Web.Models.Orders;
+
+namespace SportStore.Web.Services.Interfaces;
+
+public interface IOrderService
+{
+    /// <summary>
+    /// Sinxron elaqe - birbasa order mikroservisine istek edilicek
+    /// </summary>
+    /// <param name="checkoutInfoInput"></param>
+    /// <returns></returns>
+    Task<OrderCreatedViewModel> CreateOrder(CheckoutInfoInput checkoutInfoInput);
+
+    /// <summary>
+    /// Asinxron elaqe - sifaris melumatlari rabbitMQ-ya gonderilecek
+    /// </summary>
+    /// <param name="checkoutInfoInput"></param>
+    /// <returns></returns>
+    Task SuspendOrder(CheckoutInfoInput checkoutInfoInput);
+
+    Task<List<OrderViewModel>> GetOrder();
+}
+
