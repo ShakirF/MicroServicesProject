46261db4ab20d73b69f1580c5b69a03d4d23eaf3
Shakir <shakirfarajullayev@gmail.com>
2023-03-30 03:23:09 +0400

    ICatalogService

--- Frontends/SportStore.Web/Services/Interfaces/ICatalogService.cs
+++ Frontends/SportStore.Web/Services/Interfaces/ICatalogService.cs
@@ -7,7 +7,7 @@ public interface ICatalogService
     Task<List<ProductViewModel>> GetAllProductAsync();
     Task<List<CategoryViewModel>> GetAllCategoryAsync();
     Task<List<ProductViewModel>> GetAllProductByUserId(string userId);
-    Task<ProductViewModel> GetProductById(string productId);
+    Task<ProductViewModel> GetByProductId(string productId);
     Task<bool> CreateProductAsync(ProductCreateInput productCreateInput);
     Task<bool> UpdateProductAsync(ProductUpdateInput productUpdateInput);
     Task<bool> DeleteProductAsync(string productId);
