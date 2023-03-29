fdea4ac614e60f4bbd43518d7c470760bf969e05
Shakir <shakirfarajullayev@gmail.com>
2023-03-29 17:53:44 +0400

    DiscountApplyInputValidator

---
+++ Frontends/SportStore.Web/Validators/DiscountApplyInputValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using SportStore.Web.Models.Discounts;
+
+namespace SportStore.Web.Validators;
+
+public class DiscountApplyInputValidator : AbstractValidator<DiscountApplyInput>
+{
+    public DiscountApplyInputValidator()
+    {
+        RuleFor(x => x.Code).NotEmpty().WithMessage("endirim bos olmamalidir");
+    }
+}
+
