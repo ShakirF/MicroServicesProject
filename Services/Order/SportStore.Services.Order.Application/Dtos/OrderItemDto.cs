c890ca67223938471d0bfba058d6fdb37b02d003
Shakir <shakirfarajullayev@gmail.com>
2023-03-30 19:16:29 +0400

    OrderItemDto

--- Services/Order/SportStore.Services.Order.Application/Dtos/OrderItemDto.cs
+++ Services/Order/SportStore.Services.Order.Application/Dtos/OrderItemDto.cs
@@ -2,9 +2,9 @@
 
 public class OrderItemDto
 {
-    public string ProductId { get; set; }
-    public string ProductName { get; set; }
-    public string PictureUrl { get; set; }
+    public string ProductId { get; set; } = null!;
+    public string ProductName { get; set; } = null!;
+    public string? PictureUrl { get; set; }
     public Decimal Price { get; set; }
 }
 
