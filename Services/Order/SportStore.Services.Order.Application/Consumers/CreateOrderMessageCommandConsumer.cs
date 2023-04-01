04b6ec3288825676db59b2524e5baf8a6260be43
Shakir <shakirfarajullayev@gmail.com>
2023-04-01 04:02:22 +0400

    CreateOrderMessageCommandConsumer

---
+++ Services/Order/SportStore.Services.Order.Application/Consumers/CreateOrderMessageCommandConsumer.cs
@@ -0,0 +1,33 @@
+﻿using MassTransit;
+using SportStore.Services.Order.Infrastructure;
+using SportStore.Shared.Messages;
+
+namespace SportStore.Services.Order.Application.Consumers;
+
+public class CreateOrderMessageCommandConsumer : IConsumer<CreateOrderMessageCommand>
+{
+    private readonly OrderDbContext _orderDbContext;
+
+    public CreateOrderMessageCommandConsumer(OrderDbContext orderDbContext)
+    {
+        this._orderDbContext = orderDbContext;
+    }
+
+    public async Task Consume(ConsumeContext<CreateOrderMessageCommand> context)
+    {
+        var newAddress = new Domain.OrderAggregate.Address(context.Message.Province, context.Message.District, context.Message.Street,
+            context.Message.ZipCode, context.Message.Line);
+
+        Domain.OrderAggregate.Order order = new Domain.OrderAggregate.Order(context.Message.BuyerId, newAddress);
+
+        context.Message.OrderItems.ForEach(x =>
+        {
+            order.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureUrl);
+        });
+
+        await _orderDbContext.Orders.AddAsync(order);
+
+        await _orderDbContext.SaveChangesAsync();
+    }
+}
+
