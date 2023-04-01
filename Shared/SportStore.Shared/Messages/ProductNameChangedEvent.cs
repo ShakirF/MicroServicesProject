1e46c1191954d2bc1e7990096442cab1abe4a7e2
Shakir <shakirfarajullayev@gmail.com>
2023-04-01 04:02:55 +0400

    ProductNameChangedEvent

---
+++ Shared/SportStore.Shared/Messages/ProductNameChangedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStore.Shared.Messages
+{
+    public class ProductNameChangedEvent
+    {
+        public string ProductId { get; set; }
+        public string UpdatedName { get; set; }
+    }
+}
