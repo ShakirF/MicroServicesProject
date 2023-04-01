06fec43565d5a451075a2f8f33b8e1dc78a148e9
Shakir <shakirfarajullayev@gmail.com>
2023-04-01 04:02:32 +0400

    ProductNameChangedEventConsumer

---
+++ Services/Order/SportStore.Services.Order.Application/Consumers/ProductNameChangedEventConsumer.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using SportStore.Services.Order.Infrastructure;
+using SportStore.Shared.Messages;
+
+namespace SportStore.Services.Order.Application.Consumers;
+
+public class ProductNameChangedEventConsumer : IConsumer<ProductNameChangedEvent>
+{
+    private readonly OrderDbContext _orderDbContext;
+
+    public ProductNameChangedEventConsumer(OrderDbContext orderDbContext)
+    {
+        this._orderDbContext = orderDbContext;
+    }
+
+    public async Task Consume(ConsumeContext<ProductNameChangedEvent> context)
+    {
+        var orderItems = await _orderDbContext.OrderItems.Where(x => x.ProductId == context.Message.ProductId).ToListAsync();
+
+        orderItems.ForEach(x =>
+        {
+            x.UpdateOrderItem(context.Message.UpdatedName, x.PictureUrl, x.Price);
+        });
+        await _orderDbContext.SaveChangesAsync();
+    }
+}
+
