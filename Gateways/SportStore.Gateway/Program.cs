ab35b69d4ec00ac02abf81c4d275aabbf5af76ac
Shakir <shakirfarajullayev@gmail.com>
2023-04-02 16:20:56 +0400

    AddDelegatingHandler

--- Gateways/SportStore.Gateway/Program.cs
+++ Gateways/SportStore.Gateway/Program.cs
@@ -1,18 +1,21 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using SportStore.Gateway.DelegateHandlers;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient<TokenExcangeDelegateHandler>();
 
 builder.Configuration.AddJsonFile($"configuration.{builder.Environment.EnvironmentName.ToString().ToLower()}.json");
 
 builder.Services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", options =>
 {
-    options.Authority = builder.Configuration["IdentityServerURL"];
-    options.Audience = "resource_gateway";
-    options.RequireHttpsMetadata = false;
+	options.Authority = builder.Configuration["IdentityServerURL"];
+	options.Audience = "resource_gateway";
+	options.RequireHttpsMetadata = false;
 });
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddDelegatingHandler<TokenExcangeDelegateHandler>();
 
 
 var app = builder.Build();
