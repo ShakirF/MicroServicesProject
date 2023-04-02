8167a1915517459330687d526134ad13dfcd1403
Shakir <shakirfarajullayev@gmail.com>
2023-04-02 16:23:26 +0400

    homecontroller

--- Frontends/SportStore.Web/Controllers/HomeController.cs
+++ Frontends/SportStore.Web/Controllers/HomeController.cs
@@ -7,40 +7,48 @@ using System.Diagnostics;
 
 namespace SportStore.Web.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ICatalogService _catalogService;
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ICatalogService _catalogService;
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger, ICatalogService catalogService)
-        {
-            _logger = logger;
-            _catalogService = catalogService;
-        }
+		public HomeController(ILogger<HomeController> logger, ICatalogService catalogService)
+		{
+			_logger = logger;
+			_catalogService = catalogService;
+		}
 
-        public async Task<IActionResult> Index()
-        {
-            return View(await _catalogService.GetAllProductAsync());
-        }
+		public async Task<IActionResult> Index2()
+		{
+			return View(await _catalogService.GetAllProductAsync());
+		}
 
-        public async Task<IActionResult> Detail(string id)
-        {
-            return View(await _catalogService.GetByProductId(id));
-        }
+		public async Task<IActionResult> Index()
+		{
+			var response = await _catalogService.GetAllProductAsync();
+			var categories = await _catalogService.GetAllCategoryAsync();
+			ViewBag.Categories = categories;
+			return View(response);
+		}
+
+		public async Task<IActionResult> Detail(string id)
+		{
+			return View(await _catalogService.GetByProductId(id));
+		}
 
 
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            var errorFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			var errorFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
 
-            if (errorFeature != null && errorFeature.Error is UnAuthorizeException)
-            {
-                return RedirectToAction(nameof(AuthController.LogOut), "Auth");
-            }
+			if (errorFeature != null && errorFeature.Error is UnAuthorizeException)
+			{
+				return RedirectToAction(nameof(AuthController.LogOut), "Auth");
+			}
 
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
