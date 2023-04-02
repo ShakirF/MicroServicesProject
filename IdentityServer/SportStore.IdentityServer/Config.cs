07f07f601a5258c4448d952c7555674412108eb9
Shakir <shakirfarajullayev@gmail.com>
2023-04-02 16:21:42 +0400

    add client

--- IdentityServer/SportStore.IdentityServer/Config.cs
+++ IdentityServer/SportStore.IdentityServer/Config.cs
@@ -9,75 +9,83 @@ using System.Collections.Generic;
 
 namespace SportStore.IdentityServer
 {
-    public static class Config
-    {
-        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
-        {
-            new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
-            new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
-            new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
-            new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
-            new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
-            new ApiResource("resource_payment"){Scopes={"payment_fullpermission"}},
-            new ApiResource("resource_gateway"){Scopes={"gateway_fullpermission"}},
-            new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
-        };
+	public static class Config
+	{
+		public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+		{
+			new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
+			new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
+			new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
+			new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
+			new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
+			new ApiResource("resource_payment"){Scopes={"payment_fullpermission"}},
+			new ApiResource("resource_gateway"){Scopes={"gateway_fullpermission"}},
+			new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+		};
 
-        public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-                       new IdentityResources.Email(),
-                       new IdentityResources.OpenId(),
-                       new IdentityResources.Profile(),
-                       new IdentityResource()
-                       {Name = "roles",DisplayName = "Roles", Description = "Istifadechi rollari",
-                           UserClaims = new[]{"role"} }
-                   };
+		public static IEnumerable<IdentityResource> IdentityResources =>
+				   new IdentityResource[]
+				   {
+					   new IdentityResources.Email(),
+					   new IdentityResources.OpenId(),
+					   new IdentityResources.Profile(),
+					   new IdentityResource()
+					   {Name = "roles",DisplayName = "Roles", Description = "Istifadechi rollari",
+						   UserClaims = new[]{"role"} }
+				   };
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
-            {
-                new ApiScope("catalog_fullpermission","Catalog API ucun full access"),
-                new ApiScope("photo_stock_fullpermission","Photo Stock API ucun full access"),
-                new ApiScope("basket_fullpermission","Basket API ucun full access"),
-                new ApiScope("discount_fullpermission","Discount API ucun full access"),
-                new ApiScope("order_fullpermission","Order API ucun full access"),
-                new ApiScope("payment_fullpermission","Payment API ucun full access"),
-                new ApiScope("gateway_fullpermission","Gateway API ucun full access"),
-                new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
-            };
+		public static IEnumerable<ApiScope> ApiScopes =>
+			new ApiScope[]
+			{
+				new ApiScope("catalog_fullpermission","Catalog API ucun full access"),
+				new ApiScope("photo_stock_fullpermission","Photo Stock API ucun full access"),
+				new ApiScope("basket_fullpermission","Basket API ucun full access"),
+				new ApiScope("discount_fullpermission","Discount API ucun full access"),
+				new ApiScope("order_fullpermission","Order API ucun full access"),
+				new ApiScope("payment_fullpermission","Payment API ucun full access"),
+				new ApiScope("gateway_fullpermission","Gateway API ucun full access"),
+				new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+			};
 
-        public static IEnumerable<Client> Clients =>
-            new Client[]
-            {
-                new Client
-                {
-                    ClientName = "Asp.Net Core MVC",
-                    ClientId = "WebMvcClient",
-                    ClientSecrets = {new Secret("secret".Sha256())},
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    AllowedScopes = { "catalog_fullpermission", "photo_stock_fullpermission","gateway_fullpermission",
-                        IdentityServerConstants.LocalApi.ScopeName }
-                },
-                new Client
-                {
-                    ClientName = "Asp.Net Core MVC",
-                    ClientId = "WebMvcClientForUser",
-                    AllowOfflineAccess = true,
-                    ClientSecrets = {new Secret("secret".Sha256())},
-                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes = {"basket_fullpermission","discount_fullpermission",
-                        "order_fullpermission","payment_fullpermission","gateway_fullpermission",
-                        IdentityServerConstants.StandardScopes.Email,
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        IdentityServerConstants.StandardScopes.OfflineAccess,
-                        IdentityServerConstants.LocalApi.ScopeName,"roles" },
-                    AccessTokenLifetime = 1*60*60,
-                    RefreshTokenExpiration = TokenExpiration.Absolute,
-                    AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60) - DateTime.Now).TotalSeconds,
-                    RefreshTokenUsage = TokenUsage.ReUse
-                }
-            };
-    }
+		public static IEnumerable<Client> Clients =>
+			new Client[]
+			{
+				new Client
+				{
+					ClientName = "Asp.Net Core MVC",
+					ClientId = "WebMvcClient",
+					ClientSecrets = {new Secret("secret".Sha256())},
+					AllowedGrantTypes = GrantTypes.ClientCredentials,
+					AllowedScopes = { "catalog_fullpermission", "photo_stock_fullpermission","gateway_fullpermission",
+						IdentityServerConstants.LocalApi.ScopeName }
+				},
+				new Client
+				{
+					ClientName = "Asp.Net Core MVC",
+					ClientId = "WebMvcClientForUser",
+					AllowOfflineAccess = true,
+					ClientSecrets = {new Secret("secret".Sha256())},
+					AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+					AllowedScopes = {"basket_fullpermission",
+						"order_fullpermission","gateway_fullpermission",
+						IdentityServerConstants.StandardScopes.Email,
+						IdentityServerConstants.StandardScopes.OpenId,
+						IdentityServerConstants.StandardScopes.Profile,
+						IdentityServerConstants.StandardScopes.OfflineAccess,
+						IdentityServerConstants.LocalApi.ScopeName,"roles" },
+					AccessTokenLifetime = 1*60*60,
+					RefreshTokenExpiration = TokenExpiration.Absolute,
+					AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60) - DateTime.Now).TotalSeconds,
+					RefreshTokenUsage = TokenUsage.ReUse
+				},
+				  new Client
+				{
+					ClientName = "Token Exchange Client",
+					ClientId = "TokenExchangeClient",
+					ClientSecrets = {new Secret("secret".Sha256())},
+					AllowedGrantTypes = new [] {"urn:ietf:params:oauth:grant-type:token-exchange" },
+					AllowedScopes = { "discount_fullpermission", "payment_fullpermission", IdentityServerConstants.StandardScopes.OpenId }
+				},
+			};
+	}
 }
