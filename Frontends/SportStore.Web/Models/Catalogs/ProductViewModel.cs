2c37abd0080350635bf66f7e69163ac759a3acb1
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 21:54:07 +0400

    ProductViewModel

--- Frontends/SportStore.Web/Models/Catalogs/ProductViewModel.cs
+++ Frontends/SportStore.Web/Models/Catalogs/ProductViewModel.cs
@@ -2,25 +2,25 @@
 
 public class ProductViewModel
 {
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public string? Description { get; set; }
-    public string ShortDescription { get => Description.Length > 100 ? Description.Substring(0, 100) + "..." : Description; }
-    public decimal Price { get; set; }
+	public string Id { get; set; } = null!;
+	public string Name { get; set; } = null!;
+	public string Description { get; set; } = null!;
+	public string ShortDescription { get => Description.Length > 100 ? Description.Substring(0, 100) + "..." : Description; }
+	public decimal Price { get; set; }
 
-    public string? UserId { get; set; }
+	public string? UserId { get; set; }
 
-    public string? Picture { get; set; }
+	public string? Picture { get; set; }
 
-    public string StockPictureUrl { get; set; }
+	public string? StockPictureUrl { get; set; }
 
-    public DateTime CreatedDate { get; set; }
-    public FeatureViewModel? Feature { get; set; }
-
-
-    public string CategoryId { get; set; }
+	public DateTime CreatedDate { get; set; }
+	public FeatureViewModel? Feature { get; set; }
 
 
-    public CategoryViewModel Category { get; set; }
+	public string CategoryId { get; set; } = null!;
+
+
+	public CategoryViewModel Category { get; set; } = null!;
 }
 
