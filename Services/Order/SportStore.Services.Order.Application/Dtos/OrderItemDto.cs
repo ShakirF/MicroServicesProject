1d1ce5be8d62b928eb7caa59e88061b86d9e562c
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 22:15:22 +0400

    change nullable

--- Services/Order/SportStore.Services.Order.Application/Dtos/OrderItemDto.cs
+++ Services/Order/SportStore.Services.Order.Application/Dtos/OrderItemDto.cs
@@ -2,9 +2,9 @@
 
 public class OrderItemDto
 {
-    public string ProductId { get; set; } = null!;
-    public string ProductName { get; set; } = null!;
-    public string? PictureUrl { get; set; }
-    public Decimal Price { get; set; }
+	public string ProductId { get; set; } = null!;
+	public string ProductName { get; set; } = null!;
+	public string PictureUrl { get; set; } = null!;
+	public Decimal Price { get; set; }
 }
 
