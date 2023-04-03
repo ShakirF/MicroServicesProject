5d634cb63d553bb4513b34cfd9f7b43f5185c802
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 22:17:08 +0400

    CreateOrderMessageCommand change nullable

--- Shared/SportStore.Shared/Messages/CreateOrderMessageCommand.cs
+++ Shared/SportStore.Shared/Messages/CreateOrderMessageCommand.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 
 namespace SportStore.Shared.Messages
 {
-    public class CreateOrderMessageCommand
-    {
-        public CreateOrderMessageCommand()
-        {
-            OrderItems = new List<OrderItem>();
-        }
+	public class CreateOrderMessageCommand
+	{
+		public CreateOrderMessageCommand()
+		{
+			OrderItems = new List<OrderItem>();
+		}
 
-        public string BuyerId { get; set; }
-        public List<OrderItem> OrderItems { get; set; }
-        public string Province { get; set; }
-        public string District { get; set; }
-        public string Street { get; set; }
-        public string ZipCode { get; set; }
-        public string Line { get; set; }
+		public string BuyerId { get; set; } = null!;
+		public List<OrderItem> OrderItems { get; set; } = null!;
+		public string Province { get; set; } = null!;
+		public string District { get; set; } = null!;
+		public string Street { get; set; } = null!;
+		public string ZipCode { get; set; } = null!;
+		public string Line { get; set; } = null!;
 
-    }
+	}
 
-    public class OrderItem
-    {
-        public string ProductId { get; set; }
-        public string ProductName { get; set; }
-        public string PictureUrl { get; set; }
-        public Decimal Price { get; set; }
-    }
+	public class OrderItem
+	{
+		public string ProductId { get; set; } = null!;
+		public string ProductName { get; set; } = null!;
+		public string PictureUrl { get; set; } = null!;
+		public Decimal Price { get; set; }
+	}
 }
