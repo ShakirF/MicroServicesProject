44d3ccb1bf9ba60ed29d3322d6629c979c4cec24
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 21:49:51 +0400

    BasketController

--- Frontends/SportStore.Web/Controllers/BasketController.cs
+++ Frontends/SportStore.Web/Controllers/BasketController.cs
@@ -9,50 +9,73 @@ namespace SportStore.Web.Controllers;
 [Authorize]
 public class BasketController : Controller
 {
-    private readonly ICatalogService _catalogService;
-    private readonly IBasketService _basketService;
+	private readonly ICatalogService _catalogService;
+	private readonly IBasketService _basketService;
 
-    public BasketController(IBasketService basketService, ICatalogService catalogService)
-    {
-        this._basketService = basketService;
-        this._catalogService = catalogService;
-    }
+	public BasketController(IBasketService basketService, ICatalogService catalogService)
+	{
+		this._basketService = basketService;
+		this._catalogService = catalogService;
+	}
 
-    public async Task<IActionResult> Index()
-    {
-        return View(await _basketService.Get());
-    }
+	public async Task<IActionResult> Index()
+	{
+		return View(await _basketService.Get());
+	}
 
-    public async Task<IActionResult> AddBasketItem(string productId)
-    {
-        var product = await _catalogService.GetByProductId(productId);
-        var basketItem = new BasketItemViewModel { ProductId = product.Id, ProductName = product.Name, Price = product.Price, Quantity = 1 };
-        await _basketService.AddBasketItem(basketItem);
+	public async Task<IActionResult> AddBasketItem(string productId)
+	{
+		var product = await _catalogService.GetByProductId(productId);
+		var basketItem = new BasketItemViewModel { ProductId = product.Id, ProductName = product.Name, Price = product.Price, Quantity = 1, Picture = product.Picture, StockPictureUrl = product.StockPictureUrl };
+		await _basketService.AddBasketItem(basketItem);
 
-        return RedirectToAction(nameof(Index));
-    }
+		return RedirectToAction("Index", "Home");
+	}
 
-    public async Task<IActionResult> RemoveBasketItem(string productId)
-    {
-        await _basketService.RemoveBasketItem(productId);
-        return RedirectToAction(nameof(Index));
-    }
+	public async Task<IActionResult> RemoveBasketItem(string productId)
+	{
+		await _basketService.RemoveBasketItem(productId);
+		return RedirectToAction(nameof(Index));
+	}
+	public async Task<IActionResult> RemoveHomeBasketItem(string productId)
+	{
+		await _basketService.RemoveBasketItem(productId);
+		return RedirectToAction("Index", "Home");
+	}
 
-    public async Task<IActionResult> ApplyDiscount(DiscountApplyInput discountApplyInput)
-    {
-        if (!ModelState.IsValid)
-        {
-            TempData["discountError"] = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).First();
-        }
-        var discountStatus = await _basketService.ApplyDiscount(discountApplyInput.Code);
-        TempData["discountsStatus"] = discountStatus;
-        return RedirectToAction(nameof(Index));
-    }
+	public async Task<IActionResult> ApplyDiscount(DiscountApplyInput discountApplyInput)
+	{
+		if (!ModelState.IsValid)
+		{
+			TempData["discountError"] = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).First();
+		}
+		var discountStatus = await _basketService.ApplyDiscount(discountApplyInput.Code);
+		TempData["discountsStatus"] = discountStatus;
+		return RedirectToAction(nameof(Index));
+	}
 
-    public async Task<IActionResult> CancelApplyDiscount()
-    {
-        await _basketService.CancelApplyDiscount();
-        return RedirectToAction(nameof(Index));
-    }
+	public async Task<IActionResult> CancelApplyDiscount()
+	{
+		await _basketService.CancelApplyDiscount();
+		return RedirectToAction(nameof(Index));
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> QtyPlus(QtyData qtyData)
+	{
+		var product = await _catalogService.GetByProductId(qtyData.productId);
+		var basketItem = new BasketItemViewModel
+		{
+			ProductId = product.Id,
+			ProductName = product.Name,
+			Price = product.Price,
+			Quantity = qtyData.qty,
+			Picture = product.Picture,
+			StockPictureUrl = product.StockPictureUrl
+		};
+
+		await _basketService.AddBasketItem(basketItem);
+		return Json(qtyData);
+	}
 }
 
