bf7899be1694b7a981430ba666863fb03cadce3b
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 21:49:06 +0400

    AboutController

---
+++ Frontends/SportStore.Web/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportStore.Web.Controllers
+{
+	public class AboutController : Controller
+	{
+		public IActionResult About()
+		{
+			return View();
+		}
+	}
+}
