b0f8ebbfbcf4f87ddd1b8aa397e25fdf9b33c492
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 22:00:12 +0400

    BagViewComponent

---
+++ Frontends/SportStore.Web/ViewComponents/BagViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Web.Models.Baskets;
+using SportStore.Web.Services.Interfaces;
+
+namespace SportStore.Web.ViewComponents
+{
+
+	[ViewComponent(Name = "BagViewComponent")]
+	public class BagViewComponent : ViewComponent
+	{
+		private readonly IBasketService _basketService;
+
+		public BagViewComponent(IBasketService basketService)
+		{
+			_basketService = basketService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+
+			var response = await _basketService.Get();
+			if (response != null)
+			{
+				return View(response);
+			}
+			else
+			{
+				return View(new BasketViewModel());
+			}
+
+		}
+	}
+}
