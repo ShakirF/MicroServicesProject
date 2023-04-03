16bed3b46a5c3636ee0ca8409019855bfe7f3ec7
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 22:14:52 +0400

    CreateOrderCommand

--- Services/Order/SportStore.Services.Order.Application/Commands/CreateOrderCommand.cs
+++ Services/Order/SportStore.Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -7,10 +7,10 @@ namespace SportStore.Services.Order.Application.Commands;
 
 public class CreateOrderCommand : IRequest<Response<CreatedOrderDto>>
 {
-    public string BuyerId { get; set; }
+	public string BuyerId { get; set; } = null!;
 
-    public List<OrderItemDto> OrderItems { get; set; }
+	public List<OrderItemDto> OrderItems { get; set; } = null!;
 
-    public AddressDto Address { get; set; }
+	public AddressDto Address { get; set; } = null!;
 }
 
