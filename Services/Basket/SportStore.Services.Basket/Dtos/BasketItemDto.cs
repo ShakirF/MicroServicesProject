3f4dc060ebebcc085598cb4078639f7708c9f067
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 22:13:05 +0400

    BasketItemDto

--- Services/Basket/SportStore.Services.Basket/Dtos/BasketItemDto.cs
+++ Services/Basket/SportStore.Services.Basket/Dtos/BasketItemDto.cs
@@ -2,9 +2,11 @@
 
 public class BasketItemDto
 {
-    public int Quantity { get; set; }
-    public string ProductId { get; set; } = null!;
-    public string ProductName { get; set; } = null!;
-    public decimal Price { get; set; }
+	public int Quantity { get; set; }
+	public string? StockPictureUrl { get; set; }
+	public string? Picture { get; set; }
+	public string ProductId { get; set; } = null!;
+	public string ProductName { get; set; } = null!;
+	public decimal Price { get; set; }
 }
 
