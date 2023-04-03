778c379c3173834d324ac52024c8c44d5778c8cb
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 21:51:20 +0400

    ResourceOwnerPasswordTokenHandler

--- Frontends/SportStore.Web/Handler/ResourceOwnerPasswordTokenHandler.cs
+++ Frontends/SportStore.Web/Handler/ResourceOwnerPasswordTokenHandler.cs
@@ -7,46 +7,46 @@ namespace SportStore.Web.Handler;
 
 public class ResourceOwnerPasswordTokenHandler : DelegatingHandler
 {
-    private readonly IHttpContextAccessor _httpContextAccessor;
-    private readonly IIdentityService _identityService;
-    private readonly ILogger<ResourceOwnerPasswordTokenHandler> _logger;
+	private readonly IHttpContextAccessor _httpContextAccessor;
+	private readonly IIdentityService _identityService;
+	private readonly ILogger<ResourceOwnerPasswordTokenHandler> _logger;
 
-    public ResourceOwnerPasswordTokenHandler(
-        IHttpContextAccessor httpContextAccessor,
-        IIdentityService identityService,
-        ILogger<ResourceOwnerPasswordTokenHandler> logger)
-    {
-        this._httpContextAccessor = httpContextAccessor;
-        this._identityService = identityService;
-        this._logger = logger;
-    }
+	public ResourceOwnerPasswordTokenHandler(
+		IHttpContextAccessor httpContextAccessor,
+		IIdentityService identityService,
+		ILogger<ResourceOwnerPasswordTokenHandler> logger)
+	{
+		this._httpContextAccessor = httpContextAccessor;
+		this._identityService = identityService;
+		this._logger = logger;
+	}
 
-    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-    {
-        var accessToken = await _httpContextAccessor.HttpContext.GetTokenAsync
-            (OpenIdConnectParameterNames.AccessToken);
+	protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+	{
+		var accessToken = await _httpContextAccessor.HttpContext.GetTokenAsync
+			(OpenIdConnectParameterNames.AccessToken);
 
-        request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+		request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
 
-        var response = await base.SendAsync(request, cancellationToken);
-        if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-        {
-            var tokenResponse = await _identityService.GetAccesTokenByRefreshToken();
+		var response = await base.SendAsync(request, cancellationToken);
+		if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+		{
+			var tokenResponse = await _identityService.GetAccesTokenByRefreshToken();
 
-            if (tokenResponse == null)
-            {
-                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+			if (tokenResponse != null)
+			{
+				request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
 
-                response = await base.SendAsync(request, cancellationToken);
-            }
-        }
-        if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-        {
-            throw new UnAuthorizeException();
-        }
+				response = await base.SendAsync(request, cancellationToken);
+			}
+		}
+		if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+		{
+			throw new UnAuthorizeException();
+		}
 
-        return response;
-    }
+		return response;
+	}
 
 }
 
