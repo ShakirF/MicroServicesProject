ee49ca928930636403fcfb6f40ba6ec1fedd74e1
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 22:14:27 +0400

    PaymentDto

--- Services/FakePayment/SportStore.Services.FakePayment/Models/PaymentDto.cs
+++ Services/FakePayment/SportStore.Services.FakePayment/Models/PaymentDto.cs
@@ -2,11 +2,11 @@
 
 public class PaymentDto
 {
-    public string CardName { get; set; } = null!;
-    public string CardNumber { get; set; } = null!;
-    public string Expiration { get; set; } = null!;
-    public string CVV { get; set; } = null!;
-    public decimal TotalPrice { get; set; }
-    public OrderDto Order { get; set; } = null!;
+	public string CardName { get; set; } = null!;
+	public string CardNumber { get; set; } = null!;
+	public string Expiration { get; set; } = null!;
+	public string CVV { get; set; } = null!;
+	public decimal TotalPrice { get; set; }
+	public OrderDto Order { get; set; } = null!;
 }
 
