35990a1a6aa999f5bbf7921dcce5bac7b4dbcbb4
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 22:15:40 +0400

    GetOrdersByUserIdQuery

--- Services/Order/SportStore.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
+++ Services/Order/SportStore.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -6,6 +6,6 @@ namespace SportStore.Services.Order.Application.Queries;
 
 public class GetOrdersByUserIdQuery : IRequest<Response<List<OrderDto>>>
 {
-    public string UserId { get; set; }
+	public string? UserId { get; set; }
 }
 
