61f2e596db41b241bdf228c3f20f24679ff39b6a
Shakir <shakirfarajullayev@gmail.com>
2023-04-04 02:24:43 +0400

    FakePaymentsController

--- Services/FakePayment/SportStore.Services.FakePayment/Controllers/FakePaymentsController.cs
+++ Services/FakePayment/SportStore.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -11,38 +11,39 @@ namespace SportStore.Services.FakePayment.Controllers;
 [ApiController]
 public class FakePaymentsController : CustomBaseController
 {
-    private readonly ISendEndpointProvider _sendEndpointProvider;
+	private readonly ISendEndpointProvider _sendEndpointProvider;
 
-    public FakePaymentsController(ISendEndpointProvider sendEndpointProvider)
-    {
-        this._sendEndpointProvider = sendEndpointProvider;
-    }
+	public FakePaymentsController(ISendEndpointProvider sendEndpointProvider)
+	{
+		this._sendEndpointProvider = sendEndpointProvider;
+	}
 
-    [HttpPost]
-    public async Task<IActionResult> ReceivePaymnet(PaymentDto paymentDto)
-    {
-        var sendEndpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri("queue:create-order-service"));
+	[HttpPost]
+	public async Task<IActionResult> ReceivePaymnet(PaymentDto paymentDto)
+	{
+		var sendEndpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri("queue:create-order-service"));
 
-        var createOrderMessageCommand = new CreateOrderMessageCommand();
-        createOrderMessageCommand.BuyerId = paymentDto.Order.BuyerId;
-        createOrderMessageCommand.Province = paymentDto.Order.Address.Province;
-        createOrderMessageCommand.District = paymentDto.Order.Address.District;
-        createOrderMessageCommand.Street = paymentDto.Order.Address.Street;
-        createOrderMessageCommand.ZipCode = paymentDto.Order.Address.ZipCode;
-        createOrderMessageCommand.Line = paymentDto.Order.Address.Line;
+		var createOrderMessageCommand = new CreateOrderMessageCommand();
+		createOrderMessageCommand.BuyerId = paymentDto.Order.BuyerId;
+		createOrderMessageCommand.Province = paymentDto.Order.Address.Province;
+		createOrderMessageCommand.District = paymentDto.Order.Address.District;
+		createOrderMessageCommand.Street = paymentDto.Order.Address.Street;
+		createOrderMessageCommand.ZipCode = paymentDto.Order.Address.ZipCode;
+		createOrderMessageCommand.Line = paymentDto.Order.Address.Line;
 
-        paymentDto.Order.OrderItems.ForEach(x =>
-        {
-            createOrderMessageCommand.OrderItems.Add(new OrderItem
-            {
-                PictureUrl = x.PictureUrl,
-                Price = x.Price,
-                ProductId = x.ProductId,
-                ProductName = x.ProductName
-            });
-        });
-        await sendEndpoint.Send<CreateOrderMessageCommand>(createOrderMessageCommand);
-        return CreateActionResultInstance(Shared.Dtos.Response<NoContent>.Success(200));
-    }
+		paymentDto.Order.OrderItems.ForEach(x =>
+		{
+			createOrderMessageCommand.OrderItems.Add(new OrderItem
+			{
+				PictureUrl = x.PictureUrl,
+				Price = x.Price,
+				ProductId = x.ProductId,
+				Quantity = x.Quantity,
+				ProductName = x.ProductName
+			});
+		});
+		await sendEndpoint.Send<CreateOrderMessageCommand>(createOrderMessageCommand);
+		return CreateActionResultInstance(Shared.Dtos.Response<NoContent>.Success(200));
+	}
 }
 
