5085cb0bb50c5b14cfc7866d226a44744dfe99bf
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 21:56:38 +0400

    Client

--- Frontends/SportStore.Web/Models/ClientSettings.cs
+++ Frontends/SportStore.Web/Models/ClientSettings.cs
@@ -2,14 +2,14 @@
 
 public class ClientSettings
 {
-    public Client WebClient { get; set; } = null!;
-    public Client WebClientForUser { get; set; } = null!;
+	public Client WebClient { get; set; } = null!;
+	public Client WebClientForUser { get; set; } = null!;
 
 }
 
 public class Client
 {
-    public string ClientId { get; set; } = string.Empty;
-    public string ClientSecret { get; set; } = string.Empty;
+	public string ClientId { get; set; } = null!;
+	public string ClientSecret { get; set; } = null!;
 }
 
