10bee2b2c11408e6abc81e580eb33ffc011d36ee
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 22:11:40 +0400

    IdentityResourceOwnerPasswordValidator

--- IdentityServer/SportStore.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
+++ IdentityServer/SportStore.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -7,41 +7,41 @@ using System.Threading.Tasks;
 
 namespace SportStore.IdentityServer.Services
 {
-    public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
-    {
-        private readonly UserManager<ApplicationUser> _userManager;
+	public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
 
-        public IdentityResourceOwnerPasswordValidator(UserManager<ApplicationUser> userManager)
-        {
-            _userManager = userManager;
-        }
+		public IdentityResourceOwnerPasswordValidator(UserManager<ApplicationUser> userManager)
+		{
+			_userManager = userManager;
+		}
 
-        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
-        {
-            var existUser = await _userManager.FindByEmailAsync(context.UserName);
+		public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+		{
+			var existUser = await _userManager.FindByEmailAsync(context.UserName);
 
-            if (existUser == null)
-            {
-                var errors = new Dictionary<string, object>();
-                errors.Add("errors", new List<string> { "Email ve ya shifreniz yalnish" });
-                context.Result.CustomResponse = errors;
+			if (existUser == null)
+			{
+				var errors = new Dictionary<string, object>();
+				errors.Add("errors", new List<string> { "Password or Email incorrect" });
+				context.Result.CustomResponse = errors;
 
-                return;
-            }
-            var passwordCheck = await _userManager.CheckPasswordAsync(existUser, context.Password);
+				return;
+			}
+			var passwordCheck = await _userManager.CheckPasswordAsync(existUser, context.Password);
 
-            if (passwordCheck == false)
-            {
-                var errors = new Dictionary<string, object>();
-                errors.Add("errors", new List<string> { "Email ve ya shifreniz yalnish" });
-                context.Result.CustomResponse = errors;
+			if (passwordCheck == false)
+			{
+				var errors = new Dictionary<string, object>();
+				errors.Add("errors", new List<string> { "Password or Email incorrect" });
+				context.Result.CustomResponse = errors;
 
-                return;
-            }
+				return;
+			}
 
-            context.Result = new GrantValidationResult(existUser.Id.ToString(),
-                OidcConstants.AuthenticationMethods.Password);
+			context.Result = new GrantValidationResult(existUser.Id.ToString(),
+				OidcConstants.AuthenticationMethods.Password);
 
-        }
-    }
+		}
+	}
 }
