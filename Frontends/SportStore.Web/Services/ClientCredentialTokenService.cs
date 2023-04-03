ccedc472cc1d4210565a6f3c543935af4cfc0c30
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 21:59:12 +0400

    ClientCredentialTokenService

--- Frontends/SportStore.Web/Services/ClientCredentialTokenService.cs
+++ Frontends/SportStore.Web/Services/ClientCredentialTokenService.cs
@@ -8,55 +8,55 @@ namespace SportStore.Web.Services;
 
 public class ClientCredentialTokenService : IClientCredentialTokenService
 {
-    private readonly ServiceApiSettings _serviceApiSettings;
-    private readonly ClientSettings _clientSettings;
-    private readonly IClientAccessTokenCache _clientAccessTokenCache;
-    private readonly HttpClient _httpClient;
+	private readonly ServiceApiSettings _serviceApiSettings;
+	private readonly ClientSettings _clientSettings;
+	private readonly IClientAccessTokenCache _clientAccessTokenCache;
+	private readonly HttpClient _httpClient;
 
-    public ClientCredentialTokenService(IOptions<ServiceApiSettings> serviceApiSettings,
-        IOptions<ClientSettings> clientSettings, IClientAccessTokenCache clientAccessTokenCache,
-        HttpClient httpClient)
-    {
-        _serviceApiSettings = serviceApiSettings.Value;
-        _clientSettings = clientSettings.Value;
-        _clientAccessTokenCache = clientAccessTokenCache;
-        _httpClient = httpClient;
-    }
+	public ClientCredentialTokenService(IOptions<ServiceApiSettings> serviceApiSettings,
+		IOptions<ClientSettings> clientSettings, IClientAccessTokenCache clientAccessTokenCache,
+		HttpClient httpClient)
+	{
+		_serviceApiSettings = serviceApiSettings.Value;
+		_clientSettings = clientSettings.Value;
+		_clientAccessTokenCache = clientAccessTokenCache;
+		_httpClient = httpClient;
+	}
 
-    public async Task<string> GetToken()
-    {
-        var currentToken = await _clientAccessTokenCache.GetAsync("WebClientToken", null);
-        if (currentToken != null)
-        {
-            return currentToken.AccessToken;
-        }
-        var disco = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
-        {
-            Address = _serviceApiSettings.IdentityBaseUri,
-            Policy = new DiscoveryPolicy { RequireHttps = false }
-        });
+	public async Task<string> GetToken()
+	{
+		var currentToken = await _clientAccessTokenCache.GetAsync("WebClientToken", null);
+		if (currentToken != null)
+		{
+			return currentToken.AccessToken;
+		}
+		var disco = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+		{
+			Address = _serviceApiSettings.IdentityBaseUri,
+			Policy = new DiscoveryPolicy { RequireHttps = false }
+		});
 
-        if (disco.IsError)
-        {
-            throw disco.Exception;
-        }
+		if (disco.IsError)
+		{
+			throw disco.Exception;
+		}
 
-        var clientCredentialTokenRequest = new ClientCredentialsTokenRequest
-        {
-            ClientId = _clientSettings.WebClient.ClientId,
-            ClientSecret = _clientSettings.WebClient.ClientSecret,
-            Address = disco.TokenEndpoint
-        };
-        var newToken = await _httpClient.RequestClientCredentialsTokenAsync(clientCredentialTokenRequest);
-        if (newToken.IsError)
-        {
-            throw newToken.Exception;
-        }
+		var clientCredentialTokenRequest = new ClientCredentialsTokenRequest
+		{
+			ClientId = _clientSettings.WebClient.ClientId,
+			ClientSecret = _clientSettings.WebClient.ClientSecret,
+			Address = disco.TokenEndpoint
+		};
+		var newToken = await _httpClient.RequestClientCredentialsTokenAsync(clientCredentialTokenRequest);
+		if (newToken.IsError)
+		{
+			throw newToken.Exception;
+		}
 
-        await _clientAccessTokenCache.SetAsync("WebClientToken", newToken.AccessToken, newToken.ExpiresIn, null);
+		await _clientAccessTokenCache.SetAsync("WebClientToken", newToken.AccessToken, newToken.ExpiresIn, null);
 
-        return newToken.AccessToken;
+		return newToken.AccessToken;
 
-    }
+	}
 }
 
