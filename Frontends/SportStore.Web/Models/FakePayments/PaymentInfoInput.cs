1c13fe7ad50d221d162c2d425cacea002225cd1c
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 21:55:18 +0400

    PaymentInfoInput

--- Frontends/SportStore.Web/Models/FakePayments/PaymentInfoInput.cs
+++ Frontends/SportStore.Web/Models/FakePayments/PaymentInfoInput.cs
@@ -4,11 +4,11 @@ namespace SportStore.Web.Models.FakePayments;
 
 public class PaymentInfoInput
 {
-    public string CardName { get; set; } = null!;
-    public string CardNumber { get; set; } = null!;
-    public string Expiration { get; set; } = null!;
-    public string CVV { get; set; } = null!;
-    public decimal TotalPrice { get; set; }
-    public OrderCreateInput Order { get; set; }
+	public string CardName { get; set; } = null!;
+	public string CardNumber { get; set; } = null!;
+	public string Expiration { get; set; } = null!;
+	public string CVV { get; set; } = null!;
+	public decimal TotalPrice { get; set; }
+	public OrderCreateInput Order { get; set; } = null!;
 }
 
