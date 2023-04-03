b759277681a2b23469eb00a715b807c84607853f
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 21:53:26 +0400

    ProductCreateInput

--- Frontends/SportStore.Web/Models/Catalogs/ProductCreateInput.cs
+++ Frontends/SportStore.Web/Models/Catalogs/ProductCreateInput.cs
@@ -4,23 +4,23 @@ namespace SportStore.Web.Models.Catalogs;
 
 public class ProductCreateInput
 {
-    [Display(Name = "Məhsulun adı")]
-    public string Name { get; set; } = null!;
+	[Display(Name = "Product")]
+	public string Name { get; set; } = null!;
 
-    [Display(Name = "Məhsulun açıqlaması")]
-    public string? Description { get; set; }
+	[Display(Name = "Description")]
+	public string? Description { get; set; }
 
-    [Display(Name = "Məhsulun qiyməti")]
-    public decimal Price { get; set; }
-    public string? Picture { get; set; }
-    public string? UserId { get; set; }
-    public FeatureViewModel? Feature { get; set; }
+	[Display(Name = "Price")]
+	public decimal Price { get; set; }
+	public string? Picture { get; set; }
+	public string? UserId { get; set; }
+	public FeatureViewModel Feature { get; set; } = null!;
 
 
-    [Display(Name = "Məhsulun kateqoriyası")]
-    public string CategoryId { get; set; } = null!;
+	[Display(Name = "Category")]
+	public string CategoryId { get; set; } = null!;
 
-    [Display(Name = "Məhsulun şəkli")]
-    public IFormFile PhotoFormFile { get; set; }
+	[Display(Name = "Picture")]
+	public IFormFile PhotoFormFile { get; set; } = null!;
 }
 
