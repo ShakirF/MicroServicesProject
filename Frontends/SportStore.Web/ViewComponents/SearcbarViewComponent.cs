b1074a71af0c78cdf1c754333a088021e4edae00
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 22:00:23 +0400

    SearcbarViewComponent

---
+++ Frontends/SportStore.Web/ViewComponents/SearcbarViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Web.Services.Interfaces;
+
+namespace SportStore.Web.ViewComponents
+{
+	[ViewComponent(Name = "SearcbarViewComponent")]
+	public class SearcbarViewComponent : ViewComponent
+	{
+		private readonly ICatalogService _catalogService;
+
+		public SearcbarViewComponent(ICatalogService catalogService)
+		{
+			_catalogService = catalogService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var response = await _catalogService.GetAllCategoryAsync();
+
+			return View(response);
+		}
+	}
+}
