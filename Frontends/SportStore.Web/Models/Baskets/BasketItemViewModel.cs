772c5794803d66b860fc4e51e82c231a6e4e0024
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 21:51:54 +0400

    BasketItemViewModel

--- Frontends/SportStore.Web/Models/Baskets/BasketItemViewModel.cs
+++ Frontends/SportStore.Web/Models/Baskets/BasketItemViewModel.cs
@@ -2,20 +2,23 @@
 
 public class BasketItemViewModel
 {
-    public int Quantity { get; set; }
-    public string ProductId { get; set; }
-    public string ProductName { get; set; }
-    public decimal Price { get; set; }
+	public int Quantity { get; set; }
+	public string StockPictureUrl { get; set; } = null!;
+	public string Picture { get; set; } = null!;
+	public string ProductId { get; set; } = null!;
+	public string ProductName { get; set; } = null!;
+	public decimal Price { get; set; }
 
-    private decimal? DiscountAppliedPrice;
+	public decimal TotalEachProduct { get => GetCurrentPrice * Quantity; }
 
-    public decimal GetCurrentPrice
-    {
-        get => DiscountAppliedPrice != null ? DiscountAppliedPrice.Value : Price;
-    }
-    public void AppliedDiscount(decimal disCountPrice)
-    {
-        DiscountAppliedPrice = disCountPrice;
-    }
+	private decimal? DiscountAppliedPrice;
+	public decimal GetCurrentPrice
+	{
+		get => DiscountAppliedPrice != null ? DiscountAppliedPrice.Value : Price;
+	}
+	public void AppliedDiscount(decimal disCountPrice)
+	{
+		DiscountAppliedPrice = disCountPrice;
+	}
 }
 
