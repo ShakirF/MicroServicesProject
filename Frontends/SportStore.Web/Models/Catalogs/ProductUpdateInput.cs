05c2df7350403ce37a051f79f961c2e78d9c5f57
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 21:53:43 +0400

    ProductUpdateInput

--- Frontends/SportStore.Web/Models/Catalogs/ProductUpdateInput.cs
+++ Frontends/SportStore.Web/Models/Catalogs/ProductUpdateInput.cs
@@ -4,24 +4,24 @@ namespace SportStore.Web.Models.Catalogs;
 
 public class ProductUpdateInput
 {
-    public string Id { get; set; } = null!;
+	public string Id { get; set; } = null!;
 
-    [Display(Name = "Məhsulun adı")]
-    public string Name { get; set; } = null!;
+	[Display(Name = "Product")]
+	public string Name { get; set; } = null!;
 
-    [Display(Name = "Məhsulun açıqlaması")]
-    public string? Description { get; set; }
+	[Display(Name = "Description")]
+	public string? Description { get; set; }
 
-    [Display(Name = "Məhsulun qiyməti")]
-    public decimal Price { get; set; }
-    public string? Picture { get; set; }
-    public string? UserId { get; set; }
-    public FeatureViewModel? Feature { get; set; }
+	[Display(Name = "Price")]
+	public decimal Price { get; set; }
+	public string? Picture { get; set; }
+	public string? UserId { get; set; }
+	public FeatureViewModel Feature { get; set; } = null!;
 
-    [Display(Name = "Məhsulun kateqoriyası")]
-    public string CategoryId { get; set; } = null!;
+	[Display(Name = "Category")]
+	public string CategoryId { get; set; } = null!;
 
-    [Display(Name = "Məhsulun şəkli")]
-    public IFormFile PhotoFormFile { get; set; }
+	[Display(Name = "Picture")]
+	public IFormFile PhotoFormFile { get; set; } = null!;
 }
 
