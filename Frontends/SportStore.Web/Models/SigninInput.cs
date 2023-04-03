610fbd7693c526547d88bbd70fc92040094c2536
Shakir <shakirfarajullayev@gmail.com>
2023-04-03 21:57:12 +0400

    SigninInput

--- Frontends/SportStore.Web/Models/SigninInput.cs
+++ Frontends/SportStore.Web/Models/SigninInput.cs
@@ -4,16 +4,16 @@ namespace SportStore.Web.Models;
 
 public class SigninInput
 {
-    [Required]
-    [Display(Name = "Email adresiniz")]
-    public string Email { get; set; } = null!;
+	[Required]
+	[Display(Name = "Email Address")]
+	public string Email { get; set; } = null!;
 
-    [Required]
-    [Display(Name = "Şifrəniz")]
-    public string Password { get; set; } = null!;
+	[Required]
+	[Display(Name = "Password")]
+	public string Password { get; set; } = null!;
 
-    [Required]
-    [Display(Name = "Məni xatırla")]
-    public bool IsRemember { get; set; }
+	[Required]
+	[Display(Name = "Remember me")]
+	public bool IsRemember { get; set; }
 }
 
