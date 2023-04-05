969146b53d465cc936334b9445ee9fd41e0d5c13
Shakir <shakirfarajullayev@gmail.com>
2023-04-05 05:23:25 +0400

    usercontroller

--- IdentityServer/SportStore.IdentityServer/Controllers/UserController.cs
+++ IdentityServer/SportStore.IdentityServer/Controllers/UserController.cs
@@ -11,44 +11,49 @@ using static IdentityServer4.IdentityServerConstants;
 
 namespace SportStore.IdentityServer.Controllers
 {
-    [Authorize(LocalApi.PolicyName)]
-    [Route("api/[controller]/[action]")]
-    [ApiController]
-    public class UserController : ControllerBase
-    {
-        private readonly UserManager<ApplicationUser> _userManager;
+	[Authorize(LocalApi.PolicyName)]
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
 
-        public UserController(UserManager<ApplicationUser> userManager)
-        {
-            _userManager = userManager;
-        }
+		public UserController(UserManager<ApplicationUser> userManager)
+		{
+			_userManager = userManager;
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
-        {
-            var user = new ApplicationUser { UserName = signUpDto.UserName, Email = signUpDto.Email, City = signUpDto.City };
-            var result = await _userManager.CreateAsync(user, signUpDto.Password);
-            if (!result.Succeeded)
-            {
-                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 404));
-            }
+		[HttpPost]
+		public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+		{
+			var user = new ApplicationUser
+			{
+				UserName = signUpDto.UserName,
+				Email = signUpDto.Email,
+				City = signUpDto.City
+			};
+			var result = await _userManager.CreateAsync(user, signUpDto.Password);
+			if (!result.Succeeded)
+			{
+				return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 404));
+			}
 
-            return NoContent();
-        }
+			return NoContent();
+		}
 
 
-        [HttpGet]
-        public async Task<IActionResult> GetUser()
-        {
-            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
+		[HttpGet]
+		public async Task<IActionResult> GetUser()
+		{
+			var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
 
-            if (userIdClaim == null) return BadRequest();
+			if (userIdClaim == null) return BadRequest();
 
-            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+			var user = await _userManager.FindByIdAsync(userIdClaim.Value);
 
-            if (user == null) return BadRequest();
+			if (user == null) return BadRequest();
 
-            return Ok(new { Id = user.Id, UserName = user.UserName, Email = user.Email, City = user.City });
-        }
-    }
+			return Ok(new { Id = user.Id, UserName = user.UserName, Email = user.Email, City = user.City });
+		}
+	}
 }
