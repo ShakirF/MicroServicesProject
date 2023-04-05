7b7301c62d14f4c480f7ef3d60f789e55d165b98
Shakir <shakirfarajullayev@gmail.com>
2023-04-05 05:21:24 +0400

    RegisterService

---
+++ Frontends/SportStore.Web/Services/RegisterService.cs
@@ -0,0 +1,23 @@
+﻿using SportStore.Web.Models;
+using SportStore.Web.Services.Interfaces;
+
+namespace SportStore.Web.Services;
+
+public class RegisterService : IRegisterService
+{
+	private readonly HttpClient _httpClient;
+
+	public RegisterService(HttpClient httpClient)
+	{
+		this._httpClient = httpClient;
+	}
+
+	public async Task<bool> SignUp(SignUpInput signUpInput)
+	{
+
+		var response = await _httpClient.PostAsJsonAsync<SignUpInput>("/api/user/signup", signUpInput);
+
+		return response.IsSuccessStatusCode;
+	}
+}
+
